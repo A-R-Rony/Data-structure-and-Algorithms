9f545e66ead9002ac807ce62cedbf32b966a0765
A-R-Rony <73210651+A-R-Rony@users.noreply.github.com>
2024-04-19 18:37:55 +0600

    Create E. Exponentiation.cpp

---
+++ Others/about __int128/solved problem/E. Exponentiation.cpp
@@ -0,0 +1,123 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define IOS ios_base::sync_with_stdio(0);  cin.tie(0); cout.tie(0);
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>order_set;
+typedef pair<int, int>pr;
+#define all(i)     i.begin() , i.end()
+#define ft     first
+#define sn     second
+#define pb     push_back
+#define totalone(mask)  __builtin_popcount(mask)
+#define chkbit(mask,bit)  (mask&(1LL << bit))
+
+// debug section start
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+// debug section closed
+
+#define en "\n"
+#define sum(n) ((1LL*(n)*(n+1))/ 2LL)
+#define sqr(n) (1LL*(n)*(n))
+#define vag(a,b) ((a + b - 1)/b)
+
+#define MAXN 200010
+#define inf 1e6+5
+const int mod = 1e9 + 7;
+
+unsigned long long int a,b,m;
+unordered_map<unsigned long long int, unsigned __int128> ma;
+
+void print(unsigned __int128 x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) print(x / 10);
+    putchar(x % 10 + '0');
+}
+
+unsigned __int128 f(unsigned long long int x)
+{
+    if (x == 1) {
+        return a % m;
+    }
+    if (ma.find(x) != ma.end()) return ma[x];
+    
+    unsigned __int128 an;
+    if (x % 2 == 0) {
+        unsigned __int128 tm = f(x / 2);
+        an = tm*tm; an %= m;
+        an -= 2;
+        an += m;
+        an %= m;
+    }
+    else {
+        unsigned __int128 tm = f(x / 2);
+        unsigned __int128 tm2 = f((x / 2) + 1);
+       
+        an = tm*tm2; an %= m;
+        an -= (a % m); // an -= alpha
+        an += m;
+        an %= m;
+    }
+
+    ma[x] = an;
+
+    return an;
+}
+
+
+void solve()
+{
+    cin >> a >> b >> m;
+    // x^b + 1/x^b
+    unsigned __int128 an = f(b);
+
+    print(an);
+}
+int main()
+{
+    IOS;
+    ll t;
+    t = 1;
+    // cin >> t;
+
+    int c = 0;
+    while ( t-- )
+    {
+        // cout<<"Case "<<++c<<": ";
+        solve();
+    }
+    return 0;
+}
