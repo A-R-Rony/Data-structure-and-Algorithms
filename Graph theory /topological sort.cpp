d5905eec7ac125f47ec81f18e05a7ed811d2744d
Md. Abdur Rhaman Rony <73210651+A-R-Rony@users.noreply.github.com>
2024-09-27 00:25:47 +0600

    Create topological sort.cpp

---
+++ Graph theory /topological sort.cpp
@@ -0,0 +1,128 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define IOS ios_base::sync_with_stdio(0);  cin.tie(0); cout.tie(0);
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>order_set;
+typedef pair<int, int>pr;
+#define all(i)     i.begin() , i.end()
+#define ft     first
+#define sn     second
+#define pb     push_back
+#define totalone(mask)  __builtin_popcount(mask)
+#define chkbit(mask,bit)  (mask&(1LL << bit))
+
+// debug section start
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+// debug section closed
+
+#define en "\n"
+#define sum(n) ((1LL*(n)*(n+1))/ 2LL)
+#define sqr(n) (1LL*(n)*(n))
+#define vag(a,b) ((a + b - 1)/b)
+
+#define MAXN 200010
+#define inf 1e6+5
+const int mod = 1e9 + 7;
+
+int n, m, vis[MAXN], in[MAXN], out[MAXN];
+vector<int>g[MAXN];
+bool cycle = false;
+
+vector<int>order;
+void dfs(int nd)
+{
+    vis[nd] = 1;
+    for (auto i : g[nd])
+    {
+        if (vis[i]) continue;
+        dfs(i);
+    }
+    order.pb(nd);
+}
+
+void solve()
+{
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++) {
+        vis[i] = 0;
+        g[i].clear();
+    }
+    order.clear();
+    for (int i = 1; i <= m; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        g[x].pb(y);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (vis[i]) continue;
+        dfs(i);
+    }
+    reverse(all(order));
+    debug(order);
+
+    vector<int>pos(n + 1);
+    for (int i = 0; i < order.size(); i++)
+    {
+        pos[order[i]] = i;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (auto j : g[i]) {
+            if (pos[i] >= pos[j]) {
+                cout << "IMPOSSIBLE\n"; return;
+            }
+        }
+    }
+
+    for (auto i : order) cout << i << " "; cout << en;
+}
+int main()
+{
+    IOS;
+    ll t;
+    t = 1;
+    // cin >> t;
+    int c = 0;
+    while ( t-- )
+    {
+        // cout<<"Case "<<++c<<": ";
+        solve();
+    }
+    return 0;
+}
+// https://cses.fi/problemset/task/1679/
